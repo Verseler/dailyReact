e0891621da483f52ad80cc2f2bdc18e8f59eb094
Verseler <handuman.verselerkerr@gmail.com>
2024-02-18 14:37:01 +0800

    custome hook practice

--- src/exercises/18-02-2024/Fetching.jsx
+++ src/exercises/18-02-2024/Fetching.jsx
@@ -1,24 +1,36 @@
 import { useEffect, useState } from "react";
+import useFetch from "./useFetch";
 
 export default function Fetching() {
-  const [cat, setCat] = useState(null);
+  const url = "https://api.thecatapi.com/v1/images/search?breed_id=abys";
+  // const data = useFetch(url); --this is best for single fetch on first load of the page
+  const [data, setData] = useState([]);
 
   useEffect(() => {
     fetchData();
   }, []);
 
+  //manual fetching
   const fetchData = async () => {
     const url = "https://api.thecatapi.com/v1/images/search?breed_id=abys";
-    const response = await fetch(url);
+    try {
+      const response = await fetch(url);
     const newData = await response.json();
-    setCat(newData);
+    setData(newData)
+    } 
+    catch (error) {
+      console.log(error)
+    }
   };
+
+
+  
 
   return (
     <>
       <h1>Random Cat</h1>
       <div className="w-64 h-64 border-4 border-red-400">
-      {cat && cat.map(cat => <img key={cat.id} src={cat.url} className="object-cover w-full h-full" />)}
+      {data && data.map(cat => <img key={cat.id} src={cat.url} className="object-cover w-full h-full" />)}
       </div>
       <button onClick={fetchData} className="px-4 py-2 bg-red-100 border-2 border-red-200 rounded-md active:scale-95">Get Random Cat</button>
     </>

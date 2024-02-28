d795b45f77f0c7046decb609c344240a90041015
Verseler <handuman.verselerkerr@gmail.com>
2024-02-28 21:33:28 +0800

    chore: change app content

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import CV from "./exercises/18-02-2024/CV"
+import TempApp from "./exercises/25-02-2024/SessionInCookies/TempApp"
+import AppDuplicate from "./exercises/25-02-2024/sessionAndCookiesPractice/AppDuplicate"
 
 function App() {
  
 
   return (
     <>
-      <CV />
+      <TempApp />
     </>
   )
 }

cd6b94ea8f0c7c3d4569df1984874fcc45b89916
Verseler <handuman.verselerkerr@gmail.com>
2024-04-22 22:25:51 +0800

    change App page

--- src/App.jsx
+++ src/App.jsx
@@ -5,13 +5,14 @@ import ExcelApp from "./exercises/29-02-2024/ExcelApp"
 import Home from "./Learnify/instructor/pages/Home"
 import CreateCourse from "./Learnify/instructor/pages/CreateCourse"
 import CreateCourseCont from "./Learnify/instructor/pages/CreateCourseCont"
+import CreateAct from "./Learnify/instructor/pages/CreateAct"
 
 function App() {
  
 
   return (
     <>
-      <CreateCourseCont />
+      <CreateAct />
     </>
   )
 }

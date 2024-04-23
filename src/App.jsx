2be97b38cb8462ee422d9760b82d336230342eb5
Verseler <handuman.verselerkerr@gmail.com>
2024-04-23 23:52:54 +0800

    chore: remove uncessesary imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,3 @@
-import CV from "./exercises/18-02-2024/CV"
-import TempApp from "./exercises/25-02-2024/SessionInCookies/TempApp"
-import AppDuplicate from "./exercises/25-02-2024/sessionAndCookiesPractice/AppDuplicate"
-import ExcelApp from "./exercises/29-02-2024/ExcelApp"
-import Home from "./Learnify/instructor/pages/Home"
-import CreateCourse from "./Learnify/instructor/pages/CreateCourse"
-import CreateCourseCont from "./Learnify/instructor/pages/CreateCourseCont"
 import CreateAct from "./Learnify/instructor/pages/CreateAct"
 
 function App() {
